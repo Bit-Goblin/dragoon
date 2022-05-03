347211d5661d16a09f40fdd0bfcdc3bd8e195939
Gregory Ballantine <gballantine@metaunix.net>
2022-05-03 00:17:50 -0400

    Cleaned up the IOUtilsTest class a bit

--- src/test/java/tech/bitgoblin/io/IOUtilsTest.java
+++ src/test/java/tech/bitgoblin/io/IOUtilsTest.java
@@ -2,16 +2,19 @@ package tech.bitgoblin.io;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 
 import java.io.File;
 
 public class IOUtilsTest {
 
+  private static String testDir = "test-temp";
+
   @Test
   public void shouldCreateDirectory() {
-    IOUtils.createDirectory("test-temp.txt");
-    assertTrue(new File("test-temp.txt").exists());
+    IOUtils.createDirectory(testDir);
+    assertTrue(new File(testDir).exists());
   }
 
   @Test
@@ -23,6 +26,11 @@ public class IOUtilsTest {
   @Test(expected=UnsupportedOperationException.class)
   public void shouldFailExpandExplicitTilda() {
     IOUtils.resolveTilda("~test");
+  }
+
+  @AfterClass
+  public static void cleanup() {
+    new File(testDir).delete();
   }
 
 }
