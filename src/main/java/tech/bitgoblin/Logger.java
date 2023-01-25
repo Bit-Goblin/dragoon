f132861cbc0577dce21f113c27899ed9abbea9ad
Gregory Ballantine <gballantine@metaunix.net>
2023-01-25 10:14:50 -0500

    Fixed log4j class not found error; Updated log4j version

--- src/main/java/tech/bitgoblin/Logger.java
+++ src/main/java/tech/bitgoblin/Logger.java
@@ -4,6 +4,6 @@ import org.apache.logging.log4j.LogManager;
 
 public class Logger {
 
-  public static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+  public static org.apache.logging.log4j.Logger logger = LogManager.getRootLogger();
 
 }
